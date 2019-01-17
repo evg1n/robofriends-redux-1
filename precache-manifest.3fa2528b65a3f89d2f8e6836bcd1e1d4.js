b8bcf41d1904f1ae82a7b7b9c04af81bb880ce3e
evg1n <evginserbest@outlook.com>
2019-01-17 23:17:48 +0300

    Updates

---
+++ precache-manifest.3fa2528b65a3f89d2f8e6836bcd1e1d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux-1/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "952e1cdfd598340e5914",
+    "url": "/robofriends-redux-1/static/js/runtime~main.952e1cdf.js"
+  },
+  {
+    "revision": "11f60765d4d2cd212104",
+    "url": "/robofriends-redux-1/static/js/main.11f60765.chunk.js"
+  },
+  {
+    "revision": "0c00d67671ec354fdb0c",
+    "url": "/robofriends-redux-1/static/js/1.0c00d676.chunk.js"
+  },
+  {
+    "revision": "11f60765d4d2cd212104",
+    "url": "/robofriends-redux-1/static/css/main.2e663e51.chunk.css"
+  },
+  {
+    "revision": "0c00d67671ec354fdb0c",
+    "url": "/robofriends-redux-1/static/css/1.593c70b8.chunk.css"
+  },
+  {
+    "revision": "d25822a2bb0478a4826d18ec26d818f6",
+    "url": "/robofriends-redux-1/index.html"
+  }
+];
