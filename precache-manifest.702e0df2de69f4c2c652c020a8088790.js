c1f123076f8c424b891db3342cb7d188bed89a0e
evg1n <evginserbest@outlook.com>
2019-01-17 23:27:20 +0300

    Updates

--- precache-manifest.702e0df2de69f4c2c652c020a8088790.js
+++ precache-manifest.702e0df2de69f4c2c652c020a8088790.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/robofriends-redux-1/static/js/runtime~main.952e1cdf.js"
   },
   {
-    "revision": "11f60765d4d2cd212104",
-    "url": "/robofriends-redux-1/static/js/main.11f60765.chunk.js"
+    "revision": "5a7db524dab92235d375",
+    "url": "/robofriends-redux-1/static/js/main.5a7db524.chunk.js"
   },
   {
     "revision": "0c00d67671ec354fdb0c",
     "url": "/robofriends-redux-1/static/js/1.0c00d676.chunk.js"
   },
   {
-    "revision": "11f60765d4d2cd212104",
+    "revision": "5a7db524dab92235d375",
     "url": "/robofriends-redux-1/static/css/main.2e663e51.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/robofriends-redux-1/static/css/1.593c70b8.chunk.css"
   },
   {
-    "revision": "d25822a2bb0478a4826d18ec26d818f6",
+    "revision": "01cbdcac49a1d3302ab821568d8905a9",
     "url": "/robofriends-redux-1/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/robofriends-redux-1/precache-manifest.3fa2528b65a3f89d2f8e6836bcd1e1d4.js"
+  "/robofriends-redux-1/precache-manifest.702e0df2de69f4c2c652c020a8088790.js"
 );
 
 workbox.clientsClaim();
